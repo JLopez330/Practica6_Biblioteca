adf039e0b60ff96ca8d59e0efb6a05e227425c1d
Jlopez33 <jlopez33@uabc.edu.mx>
2024-05-20 21:28:10 -0700

    Commit estructura basica de la clase Biblioteca

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class Biblioteca {
+    private ArrayList<ElementoBiblioteca> elementos;
+    public Biblioteca(){
+        elementos = new ArrayList<>();
+    }
+
+    public void menuPrincipalConsola(){
+        int opcion = -1;
+
+        do{
+            System.out.println("========Biblioteca=======");
+            System.out.println("[1] Registrar Articulo");
+            System.out.println("[2] Buscar Articulo");
+            System.out.println("[3] Modificar Articulo");
+            System.out.println("[4] Eliminar Articulo");
+            System.out.println("[5] Salir");
+            System.out.println("=========================");
+        }while(opcion<5);
+    }
+
+    public void crearElemento(){
+
+    }
+
+    public void buscarElemento(int id){
+
+    }
+
+    public void modificarElemento(int id){
+
+    }
+
+    public void eliminarElemento(int id){
+
+    }
+
+
 }
