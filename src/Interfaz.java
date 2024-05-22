510fc2167e7eb2c5f8ec10d64114f39f3d8141b0
LuisFPD <luis.prieto@uabc.edu.mx>
2024-05-21 19:54:44 -0700

    Una pequeña prueba con la parte gráfica del programa

---
+++ src/Interfaz.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Interfaz extends JFrame {
+    public Interfaz(){
+        this.setSize(1000,1000);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Gestión de biblioteca");
+        setLocationRelativeTo(null);
+    }
+
+    public void iniciarComponentes(){
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBackground(Color.GREEN);
+        this.getContentPane().add(panel);
+
+        JLabel etiqueta = new JLabel("sistema de gestión de biblioteca.");
+        etiqueta.setBounds(50,50,500,50);
+        etiqueta.setHorizontalAlignment(SwingConstants.CENTER);
+        etiqueta.setFont(new Font("arial",2,30));
+        panel.add(etiqueta);
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args){
+        Interfaz v1 = new Interfaz();
+        v1.setVisible(true);
+        v1.iniciarComponentes();
+
+    }
+}
